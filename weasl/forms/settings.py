e43e2ff1d2f7db366d0c3bc34df93afb656c0dac
Ryan Baker <ryan.da.baker@gmail.com>
2019-11-20 20:16:12 -0500

    Removes forms which is unused

--- weasl/forms/settings.py
+++
@@ -1,8 +0,0 @@
-from wtforms import Form, StringField, validators
-
-
-class SettingsForm(Form):
-    """A form for updating org settings."""
-    company_name = StringField('Company or Project name', [validators.Length(min=5, max=100)])
-    text_login_message = StringField('Text Login Message', [validators.Length(min=5, max=250)])
-    email_magiclink = StringField('Email Login Link', [validators.URL(require_tld=False)])
